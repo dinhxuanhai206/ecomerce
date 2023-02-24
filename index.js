9e10f948b439db7d13dcf200f136853028366165
dinhxuanhai206 <62803572+dinhxuanhai206@users.noreply.github.com>
2023-02-24 17:30:02 +0700

    add

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const POST = process.env.POST || 4000;
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/authRoute")
+
+app.use("/", (req, res)=> {
+    res.send("hello i am hai");
+})
+
+app.use('api/user', authRouter)
+
+
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.listen(POST, () => {
     console.log(`Server running at post ${POST}`);
